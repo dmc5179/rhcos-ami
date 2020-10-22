fcf9364e7b2d0e204515384684b22c554e026aff
Dan Clark <danclark@redhat.com>
2020-10-21 21:59:42 -0400

    Add script for importing and registering RHCOS with only the AWS CLI

---
+++ scripts/import_ami.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# RedHat CoreOS Version
+RHCOS_VERSION="4.5.6"
+
+# S3 Bucket where the RHCOS VMDK is located
+S3BUCKET="mybucket"
+
+# S3 ARN Prefix (Different for GovCloud and other private regions"
+S3ARN="arn:aws:s3"
+
+# Path to RHCOS VMDK in the S3 Bucket
+OBJECT_PATH="rhcos-${RHCOS_VERSION}-x86_64-aws.x86_64.vmdk"
+
+cat << EOF > containers.json
+{
+    "Description": "Red Hat CoreOS ${RHCOS_VERSION} VMDK",
+    "Format": "VMDK",
+    "UserBucket": {
+        "S3Bucket": "${S3BUCKET}",
+        "S3Key": "${OBJECT_PATH}"
+    }
+}
+EOF
+
+RET=$(aws ec2 import-snapshot --description "Red Hat CoreOS ${RHCOS_VERSION} VMDK" --disk-container "file://./containers.json")
+IMPORT_ID=$( echo "${RET}" | jq -r '.ImportTaskId')
+
+IMPORT_STATUS='active'
+COUNTER=100
+until [[ ${IMPORT_STATUS} == "completed" || ${COUNTER} -eq 0 ]]
+do
+  echo "Waiting for snapshot import to complete: ${COUNTER} tries remaining"
+  IMPORT_STATUS=$(aws ec2 describe-import-snapshot-tasks --import-tasks-ids ${IMPORT_ID} | jq -r '.ImportSnapshotTasks.SnapshotTaskDetail.Status')
+  let COUNTER-=1
+  sleep 10
+done
+
+if [[ ! ${IMPORT_STATUS} == 'completed' ]]
+then
+  echo "Snapshot import failed, aborting"
+  exit 1
+fi
+
+SNAP_ID=$(aws ec2 describe-import-snapshot-tasks --import-tasks-ids ${IMPORT_ID} | jq -r '.ImportSnapshotTasks.SnapshotTaskDetail.SnapshotId')
+
+aws ec2 register-image --name "Red Hat CoreOS ${RHCOS_VERSION}" \
+    --block-device-mappings \
+"[{\"DeviceName\": \"/dev/xvda\",\"Ebs\":{\"VolumeSize\":16,\"VolumeType\":\"gp2\",\"DeleteOnTermination\":true,\"SnapshotId\":\"snap-01dc35d89af6144e0\"}}]" \
+    --root-device-name '/dev/xvda' --architecture x86_64 \
+    --description "Red Hat CoreOS ${RHCOS_VERSION}" \
+    --virtualization-type hvm --ena-support
